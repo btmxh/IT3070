df9fd7a55d8a24b85da7600afcc406060bcb2194
btmxh <ngoduyanh.chip@gmail.com>
2024-05-29 09:33:56 +0700

    implement builtin commands

--- tinyshell/libtinyshell/builtin.h
+++ tinyshell/libtinyshell/builtin.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "parse_cmd.h"
 #include "tinyshell.h"
 
@@ -6,3 +8,8 @@ int try_run_builtin(tinyshell *shell, command_parse_result *result,
 
 int builtin_cd(tinyshell* shell, int argc, char* argv[]);
 int builtin_pwd(tinyshell* shell, int argc, char* argv[]);
+int builtin_date(tinyshell* shell, int argc, char* argv[]);
+int builtin_time(tinyshell* shell, int argc, char* argv[]);
+int builtin_exit(tinyshell* shell, int argc, char* argv[]);
+int builtin_help(tinyshell* shell, int argc, char* argv[]);
+int builtin_ls(tinyshell* shell, int argc, char* argv[]);
