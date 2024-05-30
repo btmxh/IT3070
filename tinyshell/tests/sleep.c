c77a992a8322147923807df14e5bd2d5ea6fd69a
btmxh <ngoduyanh.chip@gmail.com>
2024-05-30 17:37:35 +0700

    try to enable sleep test on windows

--- tinyshell/tests/sleep.c
+++ tinyshell/tests/sleep.c
@@ -2,8 +2,6 @@
 
 int main() {
 // ping on github actions does not work
-#if !defined(WIN32) || !defined(CI_TEST)
   run_script(ROOT_TEST "tests/scripts/sleep" SCRIPT_EXTENSION);
-#endif
   return 0;
 }
