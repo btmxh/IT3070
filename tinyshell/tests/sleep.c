30b971633051d9edd63dbd26df1ff73e88847765
btmxh <ngoduyanh.chip@gmail.com>
2024-05-30 16:25:44 +0700

    exclude sleep test from win32 ci tests

--- tinyshell/tests/sleep.c
+++ tinyshell/tests/sleep.c
@@ -1,6 +1,9 @@
 #include "run_script.h"
 
 int main() {
+// ping on github actions does not work
+#if !defined(WIN32) || !defined(CI_TEST)
   run_script("tests/scripts/sleep" SCRIPT_EXTENSION);
+#endif
   return 0;
 }
