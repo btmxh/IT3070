a384fc52fff5d98cc0e343b26ad27a3a131147f8
btmxh <ngoduyanh.chip@gmail.com>
2024-05-30 17:35:44 +0700

    fix root test stuff

--- tinyshell/tests/run_script.h
+++ tinyshell/tests/run_script.h
@@ -13,10 +13,9 @@
 #define SCRIPT_EXTENSION ".tsh"
 #endif
 
-void run_script(const char *path) {
+static inline void run_script(const char *path) {
   FILE *f = fopen(path, "r");
   POSIX_WIN32(chdir)(ROOT_TEST);
-  puts(path);
   assert(f);
   tinyshell shell;
   int r = tinyshell_new(&shell, f) && tinyshell_run(&shell);
